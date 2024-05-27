df88152ec3169a552a41003bab326f92eaac6429
Zhongheng Liu <z.liu@outlook.com.gr>
2024-05-27 13:55:57 +0300

    feat(admin): login handler method init config with location assign stub

--- src/app/admin/page.tsx
+++ src/app/admin/page.tsx
@@ -1,7 +1,25 @@
 "use client";
 export default function AdminPage() {
-  const handleAdminLoginClick = () => {
+  const handleAdminLoginClick = async () => {
+    type AdminLoginResponse = {
+      success: string;
+      authKey: string;
+      validUntil: number;
+    };
     alert("Running admin login function handler...");
+    const dataRaw = await fetch("/api/admin", {
+      method: "POST",
+      body: JSON.stringify({
+        adminName:
+          (document.getElementById("admin-name") as HTMLInputElement).value,
+        adminPassword:
+          (document.getElementById("admin-password") as HTMLInputElement).value,
+      }),
+    });
+    const requestBody = await dataRaw.json() as AdminLoginResponse;
+    if (requestBody.success) {
+      location.assign(`/admin?auth=${requestBody.authKey}`);
+    } else alert("NO");
   };
   return (
     <main>
@@ -9,7 +27,7 @@ export default function AdminPage() {
       <fieldset>
         <legend>Login credentials</legend>
         <label htmlFor="admin-name">Admin name</label>
-        <input id="admin-password"></input>
+        <input id="admin-name"></input>
         <br />
         <label htmlFor="admin-password">Admin password</label>{" "}
         <input id="admin-password"></input>
